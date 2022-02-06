e3ff59bede4ddcec5c90263c30c950b8fe12732a
nirvanius <97736067+nirvanius@users.noreply.github.com>
2022-02-06 20:11:13 +0500

    final lesson3

--- Node.php
+++ Node.php
@@ -1,37 +1,90 @@
 <?php
-	class Node implements NodeInterface
-	{
-        static $hhh;
-        // public $name;
-        static $Node;
-        // public $child;
-        public function __construct(string $name) {
-            $this->name = $name;
-            $this->getChildren();
-            $this->child = implode('', self::$Node);
-        }
-        public function __toString(): string {
-            return $this->child;
-        }
+	// class Node implements NodeInterface
+	// {
+    //     static $hhh;
+    //     static $Node;
+    //     public function __construct(string $name) {
+    //         $this->name = $name;
+    //         $this->getChildren();
+    //         $this->child = implode('+', self::$Node);
+    //     }
         
-        public function getName(): string {
-            return $this->name;
-        }
+    //     public function __toString(): string {
+    //         return $this->child;
+    //     }
+        
+    //     public function getName(): string {
+    //         return $this->name;
+    //     }
     
-        /**
-         * @return Node[]
-         */
-        public function getChildren(): array {
-            self::$hhh = self::$hhh.'+';
-            self::$Node[] =self::$hhh;
-            self::$Node[] =$this->name.'<br>';
-           return self::$Node;
-        }
+    //     /**
+    //      * @return Node[]
+    //      */
+    //     public function getChildren(): array {
+    //         // self::$hhh = self::$hhh.'+';
+    //         self::$Node[] =self::$hhh;
+    //         self::$Node[] =$this->name.'<br>';
+    //        return self::$Node;
+    //     }
     
-        public function addChild(Node $node): NodeInterface
-        {
-            // self::$hhh = self::$hhh.'+';
-            $this->node =  $node;
-            return $this->node;
-        }
+    //     public function addChild(Node $node): NodeInterface
+    //     {
+    //         self::$hhh = self::$hhh.'+';
+    //         $this->node =  $node;
+    //         return $this->node;
+    //     }
+    // }
+
+
+
+    // <?php 
+
+class Node implements NodeInterface
+{
+
+    private string $name = '';
+
+    private array $children = [];
+    
+    public function __construct(string $name)
+    {
+        $this->name = $name;
     }
+
+    public function __toString(): string
+    {
+        return self::print($this);
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return Node[]
+     */
+    public function getChildren(): array
+    {
+        return $this->children;
+    }
+
+    public function addChild(Node $node): self
+    {
+        $this->children[] = $node;
+
+        return $this;
+    }
+
+    private static function print(Node $node, $level = 1)
+    {
+        $html = str_repeat('+ ', $level) . $node->getName()."\n";
+
+        foreach ($node->getChildren() as $child) {
+            $html .= self::print($child, $level + 1);
+        }
+
+        return $html;
+    }
+
+}
